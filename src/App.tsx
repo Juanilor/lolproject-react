a18d36410fa150eee696916a538b790ac9e149c3
Juani <Juanilor@github.com>
2023-04-27 20:33:34 -0300

    Change to typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+
+const App = ({props: any}) => {
+  return (
+    <div className="App">
+      <h1>some</h1>
+    </div>
+  );
+}
+
+export default App;
